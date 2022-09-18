fdbd1ebf8ec329466a90f6c1f014445e48d28a9a
Amit Kumar <amit3.kumar@africa.airtel.com>
2022-09-18 21:20:04 +0530

    Added Components

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Home = () => {
+
+    return (
+        <div>
+            This is Home Page!
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/products/Products.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import styles from './Products.module.css';
+
+const Products = () => {
+    return (
+        <div className='styles.products'>
+            This is Products Page 
+        </div>
+    )
+}
+
+export default Products;
