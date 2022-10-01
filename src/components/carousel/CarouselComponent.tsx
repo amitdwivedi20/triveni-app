9b895f45f6f5049ed1f94e25c00837b333bb142a
Amit Kumar <amit3.kumar@africa.airtel.com>
2022-10-01 14:49:56 +0530

    Changes for carousel

--- src/components/carousel/CarouselComponent.tsx
+++ src/components/carousel/CarouselComponent.tsx
@@ -7,18 +7,41 @@ const CarouselComponent = ()=>{
      
     return(
         <div>
-             <Carousel infiniteLoop centerMode autoPlay={true} showThumbs={false}>
+             <Carousel infiniteLoop centerMode autoPlay={true} showThumbs={false} autoFocus={true}>
                 <div className={styles.slide}>
-                    <img src="https://picsum.photos/300" />
+                    <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1001/500/500" />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div className={styles.slide}>
-                    <img src="https://picsum.photos/300" />
+                <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1040/500/500" />
                     <p className="legend">Legend 2</p>
                 </div>
                 <div className={styles.slide}>
-                    <img src="https://picsum.photos/300" />
+                <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1047/500/500" />
                     <p className="legend slide-up">Legend 3</p>
+                </div>
+                <div className={styles.slide}>
+                <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1070/500/500" />
+                    <p className="legend slide-up">Legend 3</p>
+                </div>
+                <div className={styles.slide}>
+                <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1072/500/500" />
+                    <p className="legend slide-up">Legend 3</p>
+                </div>
+                <div className={styles.slide}>
+                <h2>Header 1</h2>
+                    <img src="https://picsum.photos/id/1071/500/500" />
+                    <p className="legend slide-up">Legend 3</p>
+                </div>
+                <div className={styles.slide}>
+                    <img src="https://picsum.photos/id/111/500/500" />
+                    <p className="legend slide-up">Legend 3</p>
+                    <h2>Header 1</h2>
                 </div>
             </Carousel>
         </div>
