28d8f6e897718e6c2e1d03f56cb454bebad4c53e
Amit Kumar <amit3.kumar@africa.airtel.com>
2022-10-01 17:04:06 +0530

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header';
 //import Products from './pages/products/Products';
 import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
+
 
 //lazy loading 
 const Products = React.lazy(() => import('./pages/products/Products'))
@@ -12,15 +14,17 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Suspense fallback = {<p>Loading....</p>}>
-      <Routes>
-        
+      <Suspense fallback={<p>Loading....</p>}>
+        <Routes>
           <Route path="*" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/products" element={<Products />} />
-       
-      </Routes>
+        </Routes>
       </Suspense>
+       
+        <Footer />
+      
+
     </div>
   );
 }
