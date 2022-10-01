5698b812260cb6bfe3e4f234913cb18df66670ac
Amit Kumar <amit3.kumar@africa.airtel.com>
2022-10-01 15:21:04 +0530

    Added animations

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,17 +6,25 @@ const Home = () => {
 
     return (
         <div className={styles.homePage}>
-
+            <p className='animate__animated animate__slideInLeft'>
             लाडली बिटिया की गृहस्थी में निपटाऊ नहीं टिकाऊ
-            <section className={styles.homePage__section}></section>
-            <section></section>
-            <section></section>
+            </p>
             
-            <section>
+            <section className={styles.homePage__section}>
+                <div className='animate__animated animate__fadeInUp' >
+                <h1>Coming Soon...</h1>
+                <h2>Dev in progress..</h2>
+                </div>
+                
+            </section>
+            <section className={styles.homePage__section}></section>
+            <section className={styles.homePage__section}></section>
+            
+            <section className={styles.carousel__section}>
                 <CarouselComponent/>
             </section>
-            <section></section>
-            <section></section>
+            <section className={styles.homePage__section}></section>
+            <section className={styles.homePage__section}></section>
         </div>
     )
 }
