e250cdd00fe4867763990e0a8a89abb484016a79
amitdwivedi20 <amitdwivedi20790@gmail.com>
2023-08-13 03:23:06 +0530

    Changes for contact us form

--- src/pages/contact-us/contact-us-form/CotactUsForm.tsx
+++ src/pages/contact-us/contact-us-form/CotactUsForm.tsx
@@ -1,58 +1,96 @@
-import React from "react";
+import React, { SetStateAction, useState } from "react";
 import styles from './ContactUsForm.module.css';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { Button } from "@mui/material";
+import Loader from "../../../components/loader/Loader";
 
 const ContactUsForm = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [address, setAddress] = useState('');
+    const [showLoader, setShowLoader] = useState(false);
+
+    const onSubmitHandler = () =>{
+        let object = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            city: city,
+            mobile: mobile,
+            address: address
+        }
+        console.log('Contact us form submitted --- ', object);
+        resetForm();
+    }
+    const firstNameInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setFirstName(event.target.value);
+    }
+    const lastNameInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setLastName(event.target.value);
+    }
+    const emailInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setEmail(event.target.value);
+    }
+    const cityInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setCity(event.target.value);
+    }
+    const mobileInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setMobile(event.target.value);
+    }
+    const addressInputChangeHandler = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setAddress(event.target.value);
+    }
+
+    const resetForm = () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setCity('');
+        setMobile('');
+        setAddress('');
+    }
+
 
     return (
         <div className={styles.contactusform}>
-            {/* <Box sx={{ display: 'flex', flexWrap: 'wrap',flexDirection:'column' }}> */}
+            {showLoader && <Loader />}
             <div className={styles.contactusform__row}>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="First Name" variant="outlined" />
+                    <TextField required id="outlined-basic" label="First Name" variant="outlined" value={firstName} onChange={firstNameInputChangeHandler}/>
                 </FormControl>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                    <TextField required id="outlined-basic" label="Last Name" variant="outlined" value={lastName} onChange={lastNameInputChangeHandler}/>
                 </FormControl>
             </div>
 
             <div className={styles.contactusform__row}>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={emailInputChangeHandler}/>
                 </FormControl>
 
             </div>
             <div className={styles.contactusform__row}>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="City" variant="outlined" />
+                    <TextField id="outlined-basic" label="City" variant="outlined" value={city} onChange={cityInputChangeHandler}/>
                 </FormControl>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="Mobile" variant="outlined" />
+                    <TextField id="outlined-basic" label="Mobile" variant="outlined" value={mobile} onChange={mobileInputChangeHandler}/>
                 </FormControl>
             </div>
             <div className={styles.contactusform__row}>
                 <FormControl className={styles.contactusform__row___control}>
-                    <TextField id="outlined-basic" label="Address" variant="outlined" />
+                    <TextField id="outlined-basic" label="Address" variant="outlined" value={address} onChange={addressInputChangeHandler}/>
                 </FormControl>
             </div>
             <div className={styles.contactusform__row}>
                 <div className={styles.contactusform__control__button}>
-                    <Button variant="contained" >Submit</Button>
+                    <Button variant="contained" onClick={onSubmitHandler}>Submit</Button>
                 </div>
             </div>
-            {/* </Box> */}
         </div>
     )
 }
